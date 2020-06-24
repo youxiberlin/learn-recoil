561b80276ea52732dad073a8bc4dd639008ffc66
Yuki Sato <yuki.sato@lightcurve.io>
2020-06-24 15:55:26 +0200

    Add recoil tools

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import CharacterCounter from './CharacterCounter';
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from 'recoil';
+
+const textState = atom({
+  key: 'textState', // uniqueID
+  default: '', // defaul value aka initial value
+});
+
+const CharacterCounter = () => {
+  return <div>character counter: <TextInput /></div>	
+};
+
+const TextInput = () => {
+  const [text, setText] = useRecoilState(textState);
+
+  const onChange = e => setText(e.target.value);
+
+  return (
+    <div>
+      text input
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RecoilRoot>
+      <CharacterCounter />
+    </RecoilRoot>
   );
 }
 
